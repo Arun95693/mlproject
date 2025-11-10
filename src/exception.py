f177e81279fb26bc80058d11968994c531b5d552
Arun Kumar Singh <231210025@nitdelhi.ac.in>
2025-11-10 21:52:09 +0530

    Data_ingestion

--- src/exception.py
+++ src/exception.py
@@ -12,7 +12,7 @@ def error_message_detail(error, error_detail):
     return error_message
 
 
-class CustomerException(Exception):
+class CustomException(Exception):
     def __init__(self, error_message, error_detail):
         """
         :param error_message: error message in string format
